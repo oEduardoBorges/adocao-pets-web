8ad634c581a63fc566c0f3846dbd8910f8aef6ef
edwborges <eduardo.borges19646201@gmail.com>
2023-11-17 11:43:19 -0300

    Criando o componente NAV

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { NavComponent } from './components/nav/nav.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: '', component: NavComponent, canActivate: [AuthGuard], canActivateChild: [AuthGuard]}
 ];
 
 @NgModule({
